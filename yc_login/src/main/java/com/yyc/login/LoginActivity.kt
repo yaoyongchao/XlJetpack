1a616fd932453b23cf368bd0b32f2ffe18a1cc7a
yaoyongchao <yaoyongchao3g@163.com>
2021-07-27 09:05:14 +0800

    添加携程

--- yc_login/src/main/java/com/yyc/login/LoginActivity.kt
+++ yc_login/src/main/java/com/yyc/login/LoginActivity.kt
@@ -12,7 +12,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         Log.e("aabb","-----" + loginresp + "*** ")
     }
 }
