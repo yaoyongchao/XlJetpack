3d3e494bbaff23d88adaf62bdd46323ad1489bcb
yaoyongchao <yaoyongchao3g@163.com>
2021-07-30 19:43:39 +0800

    优化代码

--- lib_base/src/main/java/com/yyc/baselib/bean/XlBaseBean.kt
+++ lib_base/src/main/java/com/yyc/baselib/bean/XlBaseBean.kt
@@ -8,14 +8,14 @@ data class XlBaseBean<T>(
     /**
      * 网络数据请求成功
      */
-    fun y(func: () -> Unit) {
+    inline fun y(func: () -> Unit) {
         if (this.status == 200) func()
     }
 
     /**
      * 网络数据请求失败
      */
-    fun n(func: () -> Unit) {
+    inline fun n(func: () -> Unit) {
         if (this.status != 200) func()
     }
 }
--- yc_login/src/main/java/com/yyc/login/LoginActivity.kt
+++ yc_login/src/main/java/com/yyc/login/LoginActivity.kt
@@ -1,29 +1,45 @@
 package com.yyc.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.yyc.baselib.bean.XlBaseBean
 import com.yyc.login.databinding.ActivityLoginBinding
 import com.yyc.login.model.LoginRespModel
-import com.yyc.login.model.LoginResponse
 import com.yyc.login.net.API
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
 
 
 class LoginActivity : AppCompatActivity() {
     val loginresp by inject<LoginRespModel>()
+    lateinit  var dialog : AlertDialog
+    lateinit var builder : AlertDialog.Builder
+
     private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         Log.e("aabb","-----" + loginresp + "*** ")
         Log.e("aabb","----:" + API)
+
+
+        builder = AlertDialog.Builder(this)
+        builder.setIcon(R.mipmap.ic_launcher)
+        builder.setTitle("标题栏")
+        builder.setMessage("打发顺丰撒风")
+
+
+        dialog = builder.create()
+
 
         binding.btnLogin.setOnClickListener {
 //            coroutineTwo()
@@ -55,22 +71,38 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun flowTwo() {
-        GlobalScope.launch {
+        val aa = GlobalScope.launch {
             flow {
                 val result = API.login("17600636720","666666")
                 log("resule: ${result.msg}")
                 emit(result)
-            }.flowOn(Dispatchers.IO)
+            }.flowOn(Dispatchers.Main)
                 .onStart {
-                    log("onStart")
+                    log("onStart：${Thread.currentThread().name}" )
+                    // 主线程中更新UI
+                    withContext(Dispatchers.Main) {
+                        if (dialog.isShowing.not())
+                            dialog.show()
+                    }
                 }
                 .onCompletion {
-                    log("onCompletion")
+                    log("onCompletion：${Thread.currentThread().name}")
+                    // 主线程中更新UI
+                    withContext(Dispatchers.Main) {
+                        if (dialog.isShowing)
+                            dialog.dismiss()
+                    }
                 }
                 .catch {
-                    log("catch== " + this)
+                    log("catch== ： ${Thread.currentThread().name}" )
+                    // 主线程中更新UI
+                    withContext(Dispatchers.Main) {
+                        if (dialog.isShowing)
+                            dialog.dismiss()
+                    }
                 }
                 .collect {
+                    log("collect:$it")
                     it.n {  }
                     if (it.status == 200) {
                         log("请求成功：$it")
@@ -79,6 +111,8 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
+
+        log("aa:$aa")
 
     }
 
